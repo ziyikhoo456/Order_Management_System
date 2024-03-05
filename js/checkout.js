baad1c8e4742414b92a2d11592480eece0ae7441
tanmoon20 <tanseemoon@gmail.com>
2024-03-05 09:36:15 +0800

    change form display method and input field

---
+++ js/checkout.js
@@ -0,0 +1,36 @@
+// function ShowHideDiv(checkbox) {
+//   var division = document.getElementById("passwordDiv");
+//   division.style.display = checkbox.checked ? "block" : "none";
+// }
+
+$(document).ready(function(){
+
+  
+
+  $(function () {
+    //unchecked checkbox when page load
+    $('#acc').prop('checked',false);
+    $('#diff-acc').prop('checked',false);
+
+
+    $(".checkout__input__checkbox").on('click', '#acc', () => {
+      showHideDivision($('#acc'),$('#passwordDiv'));
+    })
+
+    $(".checkout__input__checkbox").on('click', '#diff-acc', () => {
+      showHideDivision($('#diff-acc'),$('#shipping-address-div'));
+    })
+    
+  });
+
+  function showHideDivision(checkbox,division){
+    if($(checkbox).is(":checked")){
+      $(division).show();
+    }
+    else{
+      $(division).hide();
+    }
+  }
+  
+}); 
+  
