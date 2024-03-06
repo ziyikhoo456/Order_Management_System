0a461317d642ece9b857ab1871f834f9e999eae5
tanmoon20 <tanseemoon@gmail.com>
2024-03-06 14:09:44 +0800

    connect database

--- checkout.php
+++ checkout.php
@@ -1,3 +1,4 @@
+<?php include './config/constant.php' ?>
 <!DOCTYPE html>
 <html lang="zxx">
 
---
+++ config/constant.php
@@ -0,0 +1,13 @@
+<?php
+    //Start Session
+    // session_start();
+
+    define('SITEURL','http://localhost/ogani-master/');
+    define('LOCALHOST','localhost');
+    define('DB_USERNAME','root');
+    define('DB_PASSWORD','root');
+    define('DB_NAME','electronic_order');
+
+    $conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD,DB_NAME) or die(mysqli_error($conn)); //database connection, default is root/blank
+    // $db_select = mysqli_select_db($conn,DB_NAME) or die(mysqli_error($conn)); //selecting database
+?>
