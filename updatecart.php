4c20ec697198514251710b6152d5a8fdf69f0c0c
NubYH1016 <ng101602@gmail.com>
2024-03-25 19:44:40 +0800

    Add shoping cart module

---
+++ updatecart.php
@@ -0,0 +1,21 @@
+<?php
+require './config/constant.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the value from the form
+    $sel_query="SELECT * FROM cart WHERE custID='".$_SESSION['ID']."';";
+    $result = mysqli_query($conn,$sel_query) or die(mysqli_error($conn));
+    while($row = mysqli_fetch_assoc($result)) {
+        $newquantity = $_POST[$row['prodID']];
+        if ($newquantity == 0){
+            $delete = "DELETE FROM cart WHERE custID='".$_SESSION['ID']."' AND prodID ='".$row['prodID']."' ;";
+            mysqli_query($conn, $delete) or die(mysqli_error($conn));
+        }
+        else{
+        $update="UPDATE cart set grandTotal='".$newquantity."' WHERE custID='".$_SESSION['ID']."' AND prodID ='".$row['prodID']."' ;";
+        mysqli_query($conn, $update) or die(mysqli_error($conn));
+        }
+    }       
+    echo "Done";
+}
+?>
