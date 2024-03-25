27a85cf1d3c336b9dab2dd7ee094283fca4f8a18
NubYH1016 <ng101602@gmail.com>
2024-03-25 19:45:53 +0800

    Added discount module

---
+++ discount.php
@@ -0,0 +1,15 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve the value from the form
+    $discount = $_POST["discount"];
+    $realtotal = $_POST["realtotal"];
+        if ($discount == "elecpro40"){
+        echo $realtotal*0.4;
+        $_SESSION['discount'] = 0.4;
+        }
+        else{
+            echo "Invalid Discount Code.";
+            $_SESSION['discount'] = 0;
+        }
+}
+?>
